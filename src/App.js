bbe11ad69d68d92cf3ad49ab5aff2c799e330ceb
Alix2019 <alix1982@yandex.ru>
2024-10-23 21:25:19 +0300

    layout_main

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import logoVk from './image/logoVk.svg';
 import logoBurger from './image/logoBurger.svg';
 import lotos from './image/lotos.png';
 import develop from './image/develop.svg';
+import front from './image/front.gif';
+import front2 from './image/front2.gif';
+import designer from './image/designer.gif';
+import designer2 from './image/designer2.png';
+import designer3 from './image/disigner3.MP4';
+import designer4 from './image/designer4.gif';
+import designer5 from './image/designer4.gif';
+import designer6 from './image/designer6.gif';
+import designer7 from './image/designer7.gif';
+import designer8 from './image/designer8.gif';
 
 const App = () => {
 
@@ -29,9 +39,21 @@ const App = () => {
       </header>
       <main className='main'>
         <section className='main__develop'>
-        <img className='main__developLogoLotos' src={lotos} alt='логотип лотос'/>
-        <img className='main__developLogoDevelop' src={develop} alt='печать'/>
-        <span className='main__developText'>Эта страница находится</span>
+          <img className='main__developLogoLotos' src={lotos} alt='логотип лотос'/>
+          <img className='main__developLogoDevelop' src={develop} alt='печать'/>
+          <span className='main__developText'>Эта страница находится</span>
+          <span className='main__developText1'>Пожалуйста,зайдите позднее.</span>
+        </section>
+        <section className='main__frontend'>
+          <img className='main__frontendImg' src={front2} alt='gif-fronend' width="414" height="238"/>
+          <span className='main__frontendtext'>Frontend-разработчик...</span>
+        </section>
+        <section className='main__designer'>
+          {/* <video autoplay muted loop className='main__designerImg' poster={designer2} playsinline >
+            <source src={designer3} type="video/mp4"/>
+          </video> */}
+          <img className='main__designerImg' src={designer8} alt='gif-designer' width="419" height="431"/>
+          <span className='main__designerText'>Web-дизайнер...</span>
         </section>
       </main>
       <footer className='footer'>
